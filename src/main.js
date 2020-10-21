5ad48c5beccc10dc600d6d5588c4909dfd867fec
alvarolopez99 <48553320+alvarolopez99@users.noreply.github.com>
2020-10-21 12:37:53 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 
 var gameConfig = {
-    width: 640,
-    height: 480,
+    type: Phaser.AUTO,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        parent: 'phaser-example',
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 1920,
+        height: 1080,
+    },   
     scene: [bootScene]
-    }
+}
 
 var game = new Phaser.Game(gameConfig);
 window.focus();
