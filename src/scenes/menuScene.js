3dcb5b07fbabb8e02e76b1b74e7a841e71003752
deividmcmj <48552318+deividmcmj@users.noreply.github.com>
2020-11-08 23:54:28 +0100

    Idiomas e imágenes

--- src/scenes/menuScene.js
+++ src/scenes/menuScene.js
@@ -1,46 +1,53 @@
-
 class menuScene extends Phaser.Scene{
     constructor(){
         super("menuScene");
     }
-    preload(){
 
+    init(data){
+        this.English = data.english;
     }
+
+    preload(){
+        this.load.image('screen', 'assets/FullScreen.png');
+        this.load.image('settings', 'assets/Settings.png');
+    }
+
     create(){
-        
-        this.add.text(1650,100,"Pantalla Completa",{font : "24px"}).setInteractive().on('pointerup',()=>{
-            if (this.scale.isFullscreen)
-            {
-             
-
+        this.add.image(1750, 125, 'screen').setScale(0.35).setInteractive().on('pointerup',()=>{
+            if (this.scale.isFullscreen){
                 this.scale.stopFullscreen();
-            }
-            else
-            {
-           
-
+            } else {
                 this.scale.startFullscreen();
             }
         });
+        var fsText = this.add.text(1625,250,"Pantalla completa",{font : "24px"});
+
+        this.add.image(200, 125, 'settings').setScale(0.08).setInteractive().on('pointerup',()=>{
+            this.scene.start("settings");
+        });
+        var sText = this.add.text(150,250,"Ajustes",{font : "24px"});
 
         this.add.rectangle(960,360,800,200,0xffff00).setInteractive().on('pointerup',()=>{
             //this.setFillStyle(0xffffff);
             console.log("Click");
-            
-            
         });
-
-        this.add.text(960,360,"Cooperativo en linea",{font : "24px", color : "black"});
+        var mpText = this.add.text(960,360,"Cooperativo en linea",{font : "24px", color : "black"});
 
         this.add.rectangle(960,720,800,200,0xffff00).setInteractive().on('pointerup',()=>{
             //this.setFillStyle(0xffffff);
             console.log("Click");
-            this.scene.start("selectMode");
-            
+            this.scene.start("selectMode", {english: this.English});
         });
-        this.add.text(960,720,"Un jugador",{font : "24px",color : "black"});
+        var spText = this.add.text(960,720,"Un jugador",{font : "24px",color : "black"});
 
+        if (this.English){
+            fsText.setText("Full screen");
+            sText.setText("Settings");
+            mpText.setText("Cooperative online");
+            spText.setText("Single player");
+        }
     }
+
     update(){
         
     }
