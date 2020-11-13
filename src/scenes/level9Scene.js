4e65d362a6dc0942d4f069036d6935e6e4c48d83
pbayona <pbayonag@hotmail.com>
2020-11-13 23:07:36 +0100

    Estructura básica nivel 9
    -Falta meter placas de presion + puertas/plataformas

--- src/scenes/level9Scene.js
+++ src/scenes/level9Scene.js
@@ -31,14 +31,12 @@ class level9Scene extends Phaser.Scene{
         this.load.tilemapTiledJSON('map','assets/levels/level9.json');
     }
     create(){
-        var nextLevel = this.add.zone(1970,0,10,1920);
-
         var bg = this.add.sprite(960,540,'bg');
         bg.setScrollFactor(0);
 
         //JUGADORES
-        var iniXL = 1900;
-        var iniYL = 650;
+        var iniXL = 300;
+        var iniYL = 2600;
         var playerShape = this.add.sprite(iniXL, iniYL, 'light');
         this.anims.create({
             key: 'stopL',
@@ -59,8 +57,8 @@ class level9Scene extends Phaser.Scene{
         });
         var playerPhysics = this.physics.add.existing(playerShape, 0);
 
-        var iniXS = 1900;
-        var iniYS = 2090;
+        var iniXS = 300;
+        var iniYS = 2600;
         var playerShape2 = this.add.sprite(iniXS, iniYS, 'shadow');
         this.anims.create({
             key: 'stopS',
@@ -88,15 +86,13 @@ class level9Scene extends Phaser.Scene{
             if (this.physics.world.overlap(playerPhysics2,nextLevel)){
                 this.scene.start("level10Scene");
             }
-        });
+        }, this);
 
         this.physics.add.overlap(playerPhysics2,nextLevel,function(){
             if (this.physics.world.overlap(playerPhysics,nextLevel)){
                 this.scene.start("level10Scene");
             }
-        });
-
-        var displaceY = 1440;
+        }, this);
 
         //TILEMAP
         this.map = this.add.tilemap('map');
@@ -104,7 +100,7 @@ class level9Scene extends Phaser.Scene{
         var walls = this.map.createStaticLayer('Pared', tileset, 0,0);
         this.map.createStaticLayer('Suelo',tileset,0,0);
 
-        walls.setCollision([12,13,14,19,20,21]);
+        walls.setCollision([12,13,19,20]);
 
         this.physics.add.collider(walls, playerShape);
         this.physics.add.collider(walls, playerShape2);
@@ -118,14 +114,66 @@ class level9Scene extends Phaser.Scene{
         cameraMain.startFollow(playerShape);
         camera2.startFollow(playerShape2);
 
-
-        //VIDA + PINCHOS
+        //VIDA
         var hp = new Life(this, this.English, playerShape, playerShape2);
 
         //SUELO
-        /*Rellenar*/
+        var floor1 = this.add.rectangle(1700, 2700, 3800, 100, 0xff0000);
+        floor1.setAlpha(0);
+        this.physics.add.existing(floor1, 1);
+        this.physics.add.collider(playerShape, floor1);
+        this.physics.add.collider(playerShape2, floor1);
 
-        //PLATAFORMAS
+        var floor2 = this.add.rectangle(3760, 2325, 600, 100, 0xff0000);
+        floor2.setAlpha(0);
+        this.physics.add.existing(floor2, 1);
+        this.physics.add.collider(playerShape, floor2);
+        this.physics.add.collider(playerShape2, floor2);
+
+        var floor3 = this.add.rectangle(3760, 1475, 600, 100, 0xff0000);
+        floor3.setAlpha(0);
+        this.physics.add.existing(floor3, 1);
+        this.physics.add.collider(playerShape, floor3);
+        this.physics.add.collider(playerShape2, floor3);
+
+        var floor4 = this.add.rectangle(2705, 1275, 800, 100, 0xff0000);
+        floor4.setAlpha(0);
+        this.physics.add.existing(floor4, 1);
+        this.physics.add.collider(playerShape, floor4);
+        this.physics.add.collider(playerShape2, floor4);
+
+        var floor5 = this.add.rectangle(1325, 1275, 1250, 100, 0xff0000);
+        floor5.setAlpha(0);
+        this.physics.add.existing(floor5, 1);
+        this.physics.add.collider(playerShape, floor5);
+        this.physics.add.collider(playerShape2, floor5);
+
+        var floor6 = this.add.rectangle(1800, 1950, 1100, 100, 0xff0000);
+        floor6.setAlpha(0);
+        this.physics.add.existing(floor6, 1);
+        this.physics.add.collider(playerShape, floor6);
+        this.physics.add.collider(playerShape2, floor6);
+
+        //PORTAL
+        var nextLevel = this.add.zone(400,1400,100,100);
+        this.physics.add.existing(nextLevel, 1);
+
+        var portal1 = this.add.sprite(400, 1400, 'portalA').setDepth(15);
+        portal1.setScale(3,3);
+        this.physics.add.existing(portal1,1);
+        this.physics.add.collider(playerShape, portal1, function(){
+            playerShape.setPosition(portal1.x, portal1.y);
+            playerPhysics.body.setImmovable(true);
+            playerPhysics.body.moves =false;
+        });
+        this.physics.add.collider(playerShape2, portal1, function(){
+            playerShape2.setPosition(portal1.x, portal1.y);
+            playerPhysics2.body.setImmovable(true);
+            playerPhysics2.body.moves =false;
+        });
+
+
+        //PLATAFORMAS Y PLACAS
         /*Rellenar*/
 
         //CONTROL Y MOVIMIENTO
@@ -184,10 +232,10 @@ class level9Scene extends Phaser.Scene{
         keyMovement.W.on('down', function (e) {
             pressedW = true;
             if (playerProta) {
-                if (playerPhysics.body.touching.down) {
+                //if (playerPhysics.body.touching.down) {
                     playerPhysics.body.setVelocityY(-200);
                     playerShape.anims.play('jumpL', false);
-                }
+                //}
             } else {
                 if (playerPhysics2.body.touching.down) {
                     playerPhysics2.body.setVelocityY(-200);
@@ -261,9 +309,15 @@ class level9Scene extends Phaser.Scene{
                 }
             }
         });
+
+        this.nextLevel = nextLevel;
+        this.playerPhysics = playerPhysics;
+        this.playerPhysics2 = playerPhysics2;
     
     }
     update(){
+        if(this.physics.world.overlap(this.playerPhysics,this.nextLevel) && this.physics.world.overlap(this.playerPhysics2,this.nextLevel))
+            this.scene.start("menuScene", {english: this.English});
 
     }
 }
